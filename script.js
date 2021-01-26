12a79d553b6b03a9130592d9431930e3f047ce2b
dannauu <scatterman16@gmail.com>
2021-01-25 21:31:35 -0600

    added the weather data to html from api

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 $(document).ready(function () {
-// api call for restaurants by city, state
-  var queryUrl = "https://developers.zomato.com/api/v2.1/search?entity_type=city&q=kansas%20city%2C%20missouri";
-  // var city = $('')
+  var city = document.getElementById('input_text').value;
+  var searchButton = $('#search-button')
+  searchButton.on('click', function(){
+  
+// api call for city
+  var city = document.getElementById('input_text').value;
+  var queryUrl = "https://developers.zomato.com/api/v2.1/cities?q=" + city + "&count=3";
+  
   $.ajax({
     method: "GET",
     url: queryUrl,
@@ -11,10 +16,12 @@ $(document).ready(function () {
     },
     success: function (response) {
       console.log(response);
-      console.log(response.restaurants[4].restaurant.name);
-      console.log(response.restaurants[4].restaurant.menu_url);
-      console.log(response.restaurants[4].restaurant.user_rating.aggregate_rating);
-      // console.log(response.restaurants)
+      console.log(response.location_suggestions[0].name)
+      console.log(response.location_suggestions[1].name)
+      console.log(response.location_suggestions[2].name)
+
+      $('<button/>')
+     
 
       for (i = 0; i < response.restaurants.length; i++) {
           // console.log(response.restaurants[i])
@@ -24,16 +31,33 @@ $(document).ready(function () {
 
     // api call for weather for city
     const apiKey = "&appid=3e560cf76997f959e63ea4f02f69aca9&units=imperial";
-    // var city2 = document.getElementById('input html element here').value;
+    var city = document.getElementById('input_text').value;
     var queryURL2 =
-    "https://api.openweathermap.org/data/2.5/weather?q=kansas city" + apiKey;
+    "https://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey;
   $.ajax({
     method: "GET",
     url: queryURL2,
     success: function (response2) {
-      console.log(response2);
-      console.log(response2.main.temp)
-      console.log(response2.weather[0].description)
+      // console.log(response2);
+      // console.log(response2.main.temp)
+      // console.log(response2.weather[0].description)
+      $('#tempEl').text('Temperature: ' + response2.main.temp)
+      $('#precEl').text('Precipitaion: ')
+      $('#windEl').text('Wind: ' + response2.wind.speed)
+      $('#cloudEl').text('Cloud Conditions: ' + response2.weather[0].description)
+      
     },
   });
+  
+
+
+
+
+
+
+  
+})
+
 });
+
+// create three buttons, grab names from api call, attatch event handlers for each button and when one is clicked it will do a api call for the city by the id
